effbb1545b8bd185af5f4d7c078ec9cdc7680863
lvy <upwardsrwr@gmail.com>
2024-04-30 10:46:33 +0800

    feat: init

--- src/components/FireFilesbg/index.jsx
+++ src/components/FireFilesbg/index.jsx
@@ -1,5 +1,22 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+const createFireFly = () => {
+  return {
+    id: Math.random(),
+    top: `${Math.random() * 100}%`,
+    left: `${Math.random() * 100}%`,
+    animateDuration: `${Math.random() * 5 + 5}s`
+  }
+}
+
 const FireFilesbg = () => {
-  return <div className="fixed top-0 left-0 w-full h-full"></div>
+  const [fireFiles, setFireFiles] = useState([])
+
+  useEffect(() => {}, [])
+
+  return <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden"></div>
 }
 
 export default FireFilesbg
