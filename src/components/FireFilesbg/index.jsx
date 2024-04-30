640982ea004581a77b87468bff50e9c38872aba2
lvy <upwardsrwr@gmail.com>
2024-04-30 23:16:08 +0800

    feat: index

--- src/components/FireFilesbg/index.jsx
+++ src/components/FireFilesbg/index.jsx
@@ -14,9 +14,32 @@ const createFireFly = () => {
 const FireFilesbg = () => {
   const [fireFiles, setFireFiles] = useState([])
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    const addFireFile = () => {
+      const newFireFly = createFireFly()
+      setFireFiles((prevFireFiles) => [...prevFireFiles.slice(-14), newFireFly])
+    }
+    const interval = setInterval(addFireFile, 1000)
+    return () => clearInterval(interval)
+  }, [])
 
-  return <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden"></div>
+  return (
+    <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
+      {fireFiles.map((item, index) => {
+        return (
+          <div
+            key={item.id}
+            className="absolute rounded-full  w-[10px] h-[10px] bg-firefly-radial"
+            style={{
+              top: item.top,
+              left: item.left,
+              animation: `move ${item.animateDuration} infinite alternate`
+            }}
+          ></div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default FireFilesbg
